15d0204101091da058072d00f16edf5f5241ff68
fadu-jayden <cogito1016@fadutec.com>
2020-09-17 18:38:46 +0900

    FS모듈로 파일입출력 시도

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+// import axios from 'axios';
+import fs from 'fs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state={
+      text : "Nothing",
+      number : 0
+    };
+    this.getData();
+  }//cons() end
+
+  getData(){
+    let data = fs.readFileSync('/datas/test.txt','utf-8');
+    this.setState({text:data});
+  }//getData() end
+
+
+  render(){
+    const number = this.state.number;
+    const text = this.state.text;
+    return <div>
+      <h1>Hello</h1>
+      <h2>File DownLoad</h2>
+      <h3> {number} </h3>
+      <button onClick={()=>{this.setState({number : number+1});}}> +1 </button>
+      <h4>{text}</h4>
+    </div>;
+  }//render() end
 }
 
 export default App;
