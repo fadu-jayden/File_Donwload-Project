541108a9020ccaecfce546bc236577e771e5171d
fadu-jayden <cogito1016@fadutec.com>
2020-09-18 15:26:30 +0900

    로컬파일에 접근하기 위해 수정중 XMLHttpRequest객체를 사용해서 하는중인데 잘 안먹네.. 아마 파일 경로문제일까 싶기도함

--- src/App.js
+++ src/App.js
@@ -11,24 +11,46 @@ class App extends Component{
     };
   }//cons() end
 
-
-  //선택된 파일이 변경될때(onChange이벤트) 일어나는 콜백함수 manageFile
-  selectFile(e) {
-    let fileList = e.target.files; //Input요소에서 선택한 파일들의 집합, ArrayLike형태인 FileList로 담김
-    let file = fileList[0]; //리스트중에 제일 첫번째 요소를 반환
+  // <input type="file" onChange={(e) => this.selectFile(e)}></input> 일 경우,
+  // //선택된 파일이 변경될때(onChange이벤트) 일어나는 콜백함수 manageFile
+  // selectFile(e) {
+  //   let fileList = e.target.files; //Input요소에서 선택한 파일들의 집합, ArrayLike형태인 FileList로 담김
+  //   let file = fileList[0]; //리스트중에 제일 첫번째 요소를 반환
     
-    let reader = new FileReader(); //파일리더 객체 생성
-    this.loadFile(file, reader, this.copyFile);
+  //   let reader = new FileReader(); //파일리더 객체 생성
+  //   this.loadFile(file, reader, this.copyFile);
+  // }//manageFile() end
+
+  //  loadFile(file, reader, copyFile) {
+  //   let content = "";
+  //   reader.readAsText(file);
+  //   reader.onload = function(progressEvent) { //파일리더 객체의 읽기메소드 성공시 호출하는 함수 정의
+  //     content = reader.result;
+  //     copyFile(file.name,content);
+  //   };
+  // }//loadFile() end
+
+  loadFile(copyFile) {
+
+    let rawFile = new XMLHttpRequest();
+    let filePath = "D:/workspace/annes_order/filedownload_project/src/datas/";
+    let fileName = "test.txt";
+    let totalFileName = filePath+fileName;
+    alert("rawFile 오픈 전");
+    rawFile.open("GET",totalFileName);
+    alert("rawFile 오픈 후");
+    rawFile.onreadystatechange = function(){
+        if(rawFile.readyState === 4){
+          if(rawFile.status === 200 || rawFile.status === 0){
+            let allText = rawFile.responseText;
+            alert(allText);
+            copyFile(fileName,allText);
+          }//if end
+        }//if end
+    }//onreadystatechange() end
+
   }//manageFile() end
 
-   loadFile(file, reader, copyFile) {
-    let content = "";
-    reader.readAsText(file);
-    reader.onload = function(progressEvent) { //파일리더 객체의 읽기메소드 성공시 호출하는 함수 정의
-      content = reader.result; //여기에 Content를 받아야하는데 ... 왜 내용이 안받아지지
-      copyFile(file.name,content);
-    };
-  }//loadFile() end
 
   copyFile = (fileName,content) => {
 
@@ -50,8 +72,8 @@ class App extends Component{
       <h3> {number} </h3>
       <button onClick={()=>{this.setState({number : number+1});}}> +1 </button>
       <h4>{text}</h4>
-      
-      <input type="file" onChange={(e) => this.selectFile(e)} />
+
+      <button style={{width: '100px',height: '100px'}} type="button" onClick={(e)=>( this.loadFile(this.copyFile) ) } >경로파일을 다운로드하자</button>
     </div>;
   }//render() end
 }
