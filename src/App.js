57f8f1ff0e4968412b7357756ed44e5f2ede4555
fadu-jayden <cogito1016@fadutec.com>
2020-09-18 09:51:04 +0900

    FileReader객체를 사용하여 파일읽기 성공

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 // import axios from 'axios';
-import fs from 'fs';
 
 
 class App extends Component{
@@ -10,13 +9,21 @@ class App extends Component{
       text : "Nothing",
       number : 0
     };
-    this.getData();
   }//cons() end
 
-  getData(){
-    let data = fs.readFileSync('/datas/test.txt','utf-8');
-    this.setState({text:data});
-  }//getData() end
+  showFile = async (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+
+    reader.onload = async (e) => { 
+      const text = (e.target.result)
+      console.log(text)
+      alert(text)
+    };
+    
+    reader.readAsText(e.target.files[0])
+  }
+
 
 
   render(){
@@ -28,6 +35,8 @@ class App extends Component{
       <h3> {number} </h3>
       <button onClick={()=>{this.setState({number : number+1});}}> +1 </button>
       <h4>{text}</h4>
+      
+      <input type="file" onChange={(e) => this.showFile(e)} />
     </div>;
   }//render() end
 }
