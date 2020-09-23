611a805a1895df6813c27f9d175e636f7eb3efe9
fadu-jayden <cogito1016@fadutec.com>
2020-09-23 19:28:51 +0900

    페이지네이션이 완료됐다구!

--- src/components/fileList.js
+++ src/components/fileList.js
@@ -10,13 +10,16 @@ class ListExampleSelection extends Component{
     super();
     
     this.state = {
-      offsetSize:3,
+      pageNo:1,
+      offsetSize:5,
       getFiles : [],
       selectedFiles : [],
+      showFiles :[],
     }
     this.selectFile = this.selectFile.bind(this);
     this.downloadFiles = this.downloadFiles.bind(this);
     this.getFileList = this.getFileList.bind(this);
+    this.changePage = this.changePage.bind(this);
     this.getFileList();
   }//cons end
 
@@ -27,11 +30,24 @@ class ListExampleSelection extends Component{
       //   return {id:idNum++,title:data};
       // })
       this.setState({getFiles : response.data});
+      this.paintFileList();
     })
   }//getFileList() 
 
-  paintFileList(baseIndex) {
+  paintFileList() {
+    let totalFiles = this.state.getFiles;
+    let startIdx = (this.state.pageNo-1)*this.state.offsetSize;
+    let showFiles = [];
+    let offset = this.state.offsetSize;
 
+    for(let i=startIdx;i<offset+startIdx;i++){
+
+      if(i>=totalFiles.length)
+        break;
+
+      showFiles.push(totalFiles[i]);
+    }//for end
+    this.setState({showFiles:showFiles});
   }
 
   selectFile(e) {
@@ -59,33 +75,32 @@ class ListExampleSelection extends Component{
     });
   }//downloadFiles() end
 
-  changePage(e) {
-    console.log(e);
+  async changePage(event,data) {
+    await this.setState({pageNo:data.activePage});
+    this.paintFileList();
   }//changePage() end
 
   render(){
       const getFiles = this.state.getFiles;
-
+      const showFiles = this.state.showFiles;
       return<div id ="section_downLoad">
         <Button className="downloadBtn" onClick={this.downloadFiles}>다운로드</Button>
 
         <List id ="section_fileList" selection verticalAlign='middle'>
         <List.Item className ="fileItem" >
-            {getFiles.map((file,index)=> {
+            {showFiles.map((file,index)=> {
               return <List.Content>
               <Checkbox onChange={this.selectFile} label={file} key={index} />
               {/* {console.log(file.title+' '+file.id)} */}
-              </List.Content>}
-            )}
+              </List.Content>})}
         </List.Item>
         </List>
-
         <div id="section_pagination">
         
         <Pagination
           defaultActivePage={1}
           totalPages={Math.ceil(this.state.getFiles.length/this.state.offsetSize)}
-          onPageChange={(e)=>this.changePage(e)}
+          onPageChange={this.changePage}
         />
         </div>
       </div>;
