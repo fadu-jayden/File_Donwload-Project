c9f4823b030acf1282849869a546000fdd7ede60
fadu-jayden <cogito1016@fadutec.com>
2020-09-23 11:41:37 +0900

    파일업로드 axios로 교체 성공

--- src/App.js
+++ src/App.js
@@ -13,26 +13,45 @@ class App extends Component{
 
   }//cons() end
 
-  fileUpload() {
-    axios.post('http://localhost:8080/api/fileUpload',{
-      filename: 'temp',////
+
+  
+/* <form action="http://localhost:8080/api/fileUpload" id="form" name="form" method="post" encType="multipart/form-data">
+          <input id="targetFile" name="targetFile" type="file" onChange={this.fileSelect}></input>
+          <button type="submit" onClick={this.fileUpload}>aa</button>
+        </form>
+       */
+  fileUpload(file) {
+
+    const formData = new FormData();
+    formData.append("targetFile",file);
+
+    axios({
+      url: 'http://localhost:8080/api/fileUpload',
+      method: 'POST',
+      data: formData,
       headers: {
+        Accept: 'application/json',
         'Content-Type': 'multipart/form-data'
       }
-    })
-  }
+   }).then((response)=>alert(response.data==1?"성공!":"실패.."));
+  }//fileUpload() end
+
+  fileSelect(e) {
+    //파일리스트를 받아서
+    const file = e.target.files[0];
+    //FileUpload를 호출하면 된다.
+    this.fileUpload(file);
+  }//fileSelect() end
 
   render(){
     return <div id ="layout_basic">
       
       <div id ="layout_upload">
         <h1>FileUpload</h1>
-
-      <form action="http://localhost:8080/api/fileUpload" id="form" name="form" method="post" encType="multipart/form-data">
-          <input id="filename" name="filename" type="file"></input>
-          <button type="submit" onClick={this.fileUpload}>aa</button>
-        </form>
       </div>
+      
+      <input id="targetFile" name="targetFile" type="file" onChange={(e)=>this.fileSelect(e)}></input>
+      <button type="submit" onClick={this.fileUpload}>aa</button>
 
       <div id="layout_download">
 
@@ -45,10 +64,7 @@ class App extends Component{
             <Pagination></Pagination>
           </div>
         </div>
-
       </div>
-
-
   </div>;
   }//render() end
 }
