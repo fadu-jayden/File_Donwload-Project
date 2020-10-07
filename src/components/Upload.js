602d3f9f3dc20025d1c245f50cb0e8e7364695aa
fadu-jayden <cogito1016@fadutec.com>
2020-10-07 19:31:36 +0900

    파일명왜그래

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -170,7 +170,7 @@ class Upload extends Component{
         let a = document.createElement("a");
         a.href='http://10.10.19.32:8095/ae_fileIO/api/downloadFile/'+checkedFile;
         a.download=''+checkedFile;
-        console.log(''+checkedFile+'이름 잘 나오니');
+        console.log(`${checkedFile}`);
         a.click();
 
       //   const formData = new FormData();
